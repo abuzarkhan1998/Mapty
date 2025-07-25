d1c15f29c1098059e66db3699252360b93fb2eff
Abuzar Khan <abuzar.k@flatworldsolutions.com>
2025-07-25 11:22:14 +0530

    Displaying map on marker

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ if (navigator.geolocation) {
     function (position) {
       const { latitude, longitude } = position.coords;
       // console.log(latitude,longitude);
-      const coords = [latitude,longitude];
+      const coords = [latitude, longitude];
       const map = L.map('map').setView(coords, 13);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
@@ -24,10 +24,23 @@ if (navigator.geolocation) {
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        //    console.log(mapEvent);
+        const { lat, lng: long } = mapEvent.latlng;
+        //    console.log(lat,'sd',long)
+        L.marker([lat, long])
+          .addTo(map)
+          .bindPopup(L.popup({
+            maxWidth : 350,
+            minWidth : 100,
+            className : 'running-popup',
+            autoClose : false,
+            closeOnClick : false
+          }))
+          .setPopupContent('Workout')
+          .openPopup();
+      });
+
     },
     function () {
       alert('Maps not working!!');
