f4473cbb3f08a6b074feff78e008a69b041b7fa4
Abuzar Khan <abuzar.k@flatworldsolutions.com>
2025-07-28 14:14:33 +0530

    Create a new workout

--- script.js
+++ script.js
@@ -11,45 +11,49 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new Date();
+  id = (Date.now + '').slice(-10);
 
-class Workout{
-    date = new Date();
-    id = (Date.now + '').slice(-10);
-
-    constructor(coords,dist, dur){
-        this.coords = coords;
-        this.dist = dist;
-        this.dur = dur;
-    }
+  constructor(coords, dist, dur) {
+    this.coords = coords;
+    this.dist = dist;
+    this.dur = dur;
+  }
 }
 
-class Running extends Workout{
-    constructor(coords,dist,dur,cadence){
-        super(coords,dist,dur);
-        this.cadence = cadence;
-    }
+class Running extends Workout {
+  type = 'running';
+  constructor(coords, dist, dur, cadence) {
+    super(coords, dist, dur);
+    this.cadence = cadence;
+    console.log(this);
+  }
 
-    calcPace(){
-        this.pace = this.dur/this.dist;
-        return this.pace;
-    }
+  calcPace() {
+    this.pace = this.dur / this.dist;
+    return this.pace;
+  }
 }
 
-class Cycling extends Workout{
-    constructor(coords,dist,dur,elevation){
-        super(coords,dist,dur);
-        this.elevation = elevation;
-    }
+class Cycling extends Workout {
+  type = 'cycling';
+  constructor(coords, dist, dur, elevation) {
+    super(coords, dist, dur);
+    this.elevation = elevation;
+    console.log(this);
+  }
 
-    calcSpeed(){
-        this.speed = this.dist/(this.this.dur/60);
-        return this.speed;
-    }
+  calcSpeed() {
+    this.speed = this.dist / (this.dur / 60);
+    return this.speed;
+  }
 }
 
 class App {
   #map;
   #mapEvent;
+  #workout = [];
 
   constructor() {
     this._getPosition();
@@ -91,34 +95,75 @@ class App {
   }
 
   _toggleElevationField() {
-      inputElevation
-        .closest('.form__row')
-        .classList.toggle('form__row--hidden');
-      inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
   }
 
   _newWorkout(e) {
     e.preventDefault();
+    const workoutType = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+    let workout;
+    const { lat, lng: long } = this.#mapEvent.latlng;
+
+    //Check if data is valid
+
+    const checkValidInput = (...inputs) =>
+      inputs.every(inp => Number.isFinite(inp));
+
+    const validatePositiveNumbers = (...inputs) => inputs.every(inp => inp > 0);
+
+    //If workout is running, create running object
+    if (workoutType === 'running') {
+      const cadence = +inputCadence.value;
+    //   console.log(cadence);
+      if (
+        !checkValidInput(distance, duration, cadence) ||
+        !validatePositiveNumbers(distance, duration, cadence)
+      )
+        alert('Input fields must be a positive number');
+      workout = new Running([lat, long], distance, duration, cadence);
+    }
+
+    //If workout is cycling, create cycling object
+    if (workoutType === 'cycling') {
+      const elevation = +inputElevation.value;
+      if (
+        !checkValidInput(distance, duration, elevation) ||
+        !validatePositiveNumbers(distance, duration)
+      )
+        alert('Input fields must be a positive number');
+      workout = new Cycling([lat, long], distance, duration, elevation);
+    }
+
+    //add new object in workout array
+    this.#workout.push(workout);
+
+    //display marker on map
+    this._renderWorkoutMap(workout);
+
     inputDistance.value =
       inputDuration.value =
       inputCadence.value =
       inputElevation.value =
         '';
     //    console.log(mapEvent);
-    const { lat, lng: long } = this.#mapEvent.latlng;
-    //    console.log(lat,'sd',long)
-    L.marker([lat, long])
+  }
+
+  _renderWorkoutMap(position){
+    L.marker(position.coords)
       .addTo(this.#map)
       .bindPopup(
         L.popup({
           maxWidth: 350,
           minWidth: 100,
-          className: 'running-popup',
+          className: `${position.type}-popup`,
           autoClose: false,
           closeOnClick: false,
         })
       )
-      .setPopupContent('Workout')
+      .setPopupContent(`${position.type}`)
       .openPopup();
   }
 }
